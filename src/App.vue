5901ead09cb125138823fc1c79293c6f6a9fc944
Amilcar Celis <79593733+MolkAmilcar@users.noreply.github.com>
2022-06-26 21:59:43 -0400

    local project
    Funcionalidades completas, falta implementar pinia y api

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <to-doer></to-doer>
+</template>
+
+<script>
+import ToDoer from './components/ToDoer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToDoer
+  }
+}
+</script>
